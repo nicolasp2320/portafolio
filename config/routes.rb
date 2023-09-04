8a96e20cebf96ada780882e455ea42ede9e2cf0f
nicolas <nicolasp2320@gmail.com>
2023-09-04 20:35:36 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root 'home#index' 
+  get '/home', to: 'home#index'
+  get '/projects', to: 'projects#index'
+  get '/contact', to: 'contact#index'
+  
+  get '/carousel', to: 'projects#carousel'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
